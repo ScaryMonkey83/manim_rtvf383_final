61938b180f3207e9999e6dad1338874800532688
jdetwiler <jacob.detwiler@theperducogroup.com>
2021-12-01 21:36:51 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,138 @@
+import logging
+
+import soundfile as sf
+from manim import *
+import numpy as np
+from manim._config import config
+from manim_music import frame_rate, file_to_var
+
+
+logger = logging.getLogger(__name__)
+
+# also constants
+width = 3
+multiplier = 3
+
+# constant lists
+colors = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE]
+locs = [[-3 * width, 0, 0], [-2 * width, 0, 0], [-1 * width, 0, 0], [0 * width, 0, 0], [1 * width, 0, 0], [2 * width, 0, 0], [3 * width, 0, 0]]
+
+class Video(ThreeDScene):
+    @staticmethod
+    def getline(sphere1, sphere2):
+        start_point = sphere1.get_center()
+        end_point = sphere2.get_center()
+        line = Line3D(
+            start_point,
+            end_point)
+        return line
+
+    def define_pyramid(self, color: color.Color, xyz: list):
+        """This function defines a pyramid to be manipulated and returns"""
+        sphere1 = Sphere().set_color(color).scale(0.2).move_to((1 + xyz[0]) * RIGHT + (1 + xyz[1]) * UP + (-2 + xyz[2]) * OUT)
+        sphere2 = sphere1.copy().move_to((-1 + xyz[0]) * RIGHT + (1 + xyz[1]) * UP + (-2 + xyz[2]) * OUT)
+        sphere3 = sphere1.copy().move_to((-1 + xyz[0]) * RIGHT + (-1 + xyz[1]) * UP + (-2 + xyz[2]) * OUT)
+        sphere4 = sphere1.copy().move_to((1 + xyz[0])* RIGHT + (-1 + xyz[1]) * UP + (-2 + xyz[2]) * OUT)
+        sphere5 = sphere1.copy().move_to(xyz[0] * RIGHT + xyz[1] * UP + (-2 + xyz[2]) * OUT)
+        sphere6 = sphere5.copy().set_opacity(0.3).scale(0.1).move_to(xyz[0] * RIGHT + xyz[1] * UP + (-2 + xyz[2]) * OUT)
+
+        line1 = self.getline(sphere1, sphere2)
+        line1.add_updater(lambda mob: mob.become(self.getline(sphere1, sphere2)))
+        line2 = self.getline(sphere2, sphere3)
+        line2.add_updater(lambda mob: mob.become(self.getline(sphere2, sphere3)))
+        line3 = self.getline(sphere3, sphere4)
+        line3.add_updater(lambda mob: mob.become(self.getline(sphere3, sphere4)))
+        line4 = self.getline(sphere4, sphere1)
+        line4.add_updater(lambda mob: mob.become(self.getline(sphere4, sphere1)))
+
+        line5 = self.getline(sphere5, sphere1)
+        line5.add_updater(lambda mob: mob.become(self.getline(sphere5, sphere1)))
+        line6 = self.getline(sphere5, sphere2)
+        line6.add_updater(lambda mob: mob.become(self.getline(sphere5, sphere2)))
+        line7 = self.getline(sphere5, sphere3)
+        line7.add_updater(lambda mob: mob.become(self.getline(sphere5, sphere3)))
+        line8 = self.getline(sphere5, sphere4)
+        line8.add_updater(lambda mob: mob.become(self.getline(sphere5, sphere4)))
+
+        spheres = [sphere1, sphere2, sphere3, sphere4, sphere5, sphere6]
+        lines = [line1, line2, line3, line4, line5, line6, line7, line8]
+
+        [self.add(line) for line in lines]
+        [self.add(sphere) for sphere in spheres]
+
+        return spheres
+
+    def construct(self):
+        def process_one(pyramids: List[List[Sphere]], drum, rand_func, play=True):
+            # cross product to get the perpendicular unit vector (for each track)
+            cp0 = np.cross(pyramids[0][1].get_center() - pyramids[0][0].get_center(),
+                           pyramids[0][3].get_center() - pyramids[0][0].get_center())
+            cp1 = np.cross(pyramids[1][1].get_center() - pyramids[1][0].get_center(),
+                           pyramids[1][3].get_center() - pyramids[1][0].get_center())
+            cp2 = np.cross(pyramids[2][1].get_center() - pyramids[2][0].get_center(),
+                           pyramids[2][3].get_center() - pyramids[2][0].get_center())
+            cp3 = np.cross(pyramids[3][1].get_center() - pyramids[3][0].get_center(),
+                           pyramids[3][3].get_center() - pyramids[3][0].get_center())
+            cp4 = np.cross(pyramids[4][1].get_center() - pyramids[4][0].get_center(),
+                           pyramids[4][3].get_center() - pyramids[4][0].get_center())
+            cp5 = np.cross(pyramids[5][1].get_center() - pyramids[5][0].get_center(),
+                           pyramids[5][3].get_center() - pyramids[5][0].get_center())
+            cp6 = np.cross(pyramids[6][1].get_center() - pyramids[6][0].get_center(),
+                           pyramids[6][3].get_center() - pyramids[6][0].get_center())
+
+            # add the resulting unit vector to the center point on the base for each pyramid and animate it!
+            if play:
+                self.play(
+                    pyramids[0][4].animate.move_to(pyramids[0][5].get_center() + (cp0 / np.linalg.norm(cp0)) * multiplier * drum[0] if drum[0] > 0.4 else pyramids[0][5].get_center()),
+                    pyramids[1][4].animate.move_to(pyramids[1][5].get_center() + (cp1 / np.linalg.norm(cp1)) * multiplier * drum[1] if drum[1] > 0.4 else pyramids[1][5].get_center()),
+                    pyramids[2][4].animate.move_to(pyramids[2][5].get_center() + (cp2 / np.linalg.norm(cp2)) * multiplier * drum[2] if drum[2] > 0.4 else pyramids[2][5].get_center()),
+                    pyramids[3][4].animate.move_to(pyramids[3][5].get_center() + (cp3 / np.linalg.norm(cp3)) * multiplier * drum[3] if drum[3] > 0.4 else pyramids[3][5].get_center()),
+                    pyramids[4][4].animate.move_to(pyramids[4][5].get_center() + (cp4 / np.linalg.norm(cp4)) * multiplier * drum[4] if drum[4] > 0.4 else pyramids[4][5].get_center()),
+                    pyramids[5][4].animate.move_to(pyramids[5][5].get_center() + (cp5 / np.linalg.norm(cp5)) * multiplier * drum[5] if drum[5] > 0.4 else pyramids[5][5].get_center()),
+                    pyramids[6][4].animate.move_to(pyramids[6][5].get_center() + (cp6 / np.linalg.norm(cp6)) * multiplier * drum[6] if drum[6] > 0.4 else pyramids[6][5].get_center()),
+                    run_time=1/frame_rate,
+                    rate_func=rate_functions.linear
+                )
+            else:
+                v0_ = pyramids[0][5].get_center() + (cp0 / np.linalg.norm(cp0)) * multiplier * drum[0] if drum[0] > 0.1 else pyramids[0][5].get_center()
+                v1_ = pyramids[1][5].get_center() + (cp1 / np.linalg.norm(cp1)) * multiplier * drum[1] if drum[1] > 0.1 else pyramids[1][5].get_center()
+                v2_ = pyramids[2][5].get_center() + (cp2 / np.linalg.norm(cp2)) * multiplier * drum[2] if drum[2] > 0.1 else pyramids[2][5].get_center()
+                v3_ = pyramids[3][5].get_center() + (cp3 / np.linalg.norm(cp3)) * multiplier * drum[3] if drum[3] > 0.1 else pyramids[3][5].get_center()
+                v4_ = pyramids[4][5].get_center() + (cp4 / np.linalg.norm(cp4)) * multiplier * drum[4] if drum[4] > 0.1 else pyramids[4][5].get_center()
+                v5_ = pyramids[5][5].get_center() + (cp5 / np.linalg.norm(cp5)) * multiplier * drum[5] if drum[5] > 0.1 else pyramids[5][5].get_center()
+                v6_ = pyramids[6][5].get_center() + (cp6 / np.linalg.norm(cp6)) * multiplier * drum[6] if drum[6] > 0.1 else pyramids[6][5].get_center()
+
+                return v0_, v1_, v2_, v3_, v4_, v5_, v6_
+
+        # load designated data
+        data: np.ndarray = file_to_var(config.music_file)
+
+        # init scene data
+        light = self.camera.light_source
+        light.move_to([-25, -20, 20])
+
+        # introduction
+        if config.first:
+            pyramids = [self.define_pyramid(col, loc) for (col, loc) in zip(colors, locs)]
+            self.wait(4)
+            self.move_camera(0.45*np.pi, -0.05*np.pi)
+        else:
+            # if not the first, run it like this
+            self.set_camera_orientation(0.45 * np.pi, -0.05 * np.pi)
+            pyramids = [self.define_pyramid(col, loc) for (col, loc) in zip(colors, locs)]
+            v0, v1, v2, v3, v4, v5, v6 = process_one(pyramids, data[0, :], None, play=False)
+            data = data[1:, :]
+
+            pyramids[0][4].move_to(v0)
+            pyramids[1][4].move_to(v1)
+            pyramids[2][4].move_to(v2)
+            pyramids[3][4].move_to(v3)
+            pyramids[4][4].move_to(v4)
+            pyramids[5][4].move_to(v5)
+            pyramids[6][4].move_to(v6)
+
+        # main loop
+        for sample in range(data.shape[0]):
+            # sample = sample + int(2 * data.shape[0] / 3)
+            d = data[sample, :]
+            process_one(pyramids, d, None)
---
+++ manim_music.py
@@ -0,0 +1,130 @@
+import os
+from datetime import datetime
+import pickle
+
+from cloup import (
+    command, option, option_group
+)
+import soundfile as sf
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+import numpy as np
+
+
+# constants
+file = "audio"
+tmp = "tmp"
+n_cores = 12
+window_multiplier = 4
+frame_rate = 15
+qual_flag = '-pql'
+
+
+def var_to_file(var, path):
+    with open(path, 'wb') as f:
+        pickle.dump(var, f)
+
+def file_to_var(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+def window_rms(a, window_sz):
+    if type(window_sz) is float:
+        window_sz = int(window_sz)
+    a2 = np.power(a, 2)
+    window = np.ones(window_sz) / float(window_sz)
+    return np.sqrt(np.convolve(a2, window, 'valid'))
+
+
+# constructs the video from audio asynchronously
+if __name__ == '__main__':
+    now = datetime.now()
+
+    # remove files before fs setup
+    os.system('rm -r debug')
+    os.system('rm -r tmp')
+
+    # fs setup
+    os.system('mkdir debug')
+    os.system('mkdir {}'.format(tmp))
+    os.system('mkdir {}/media'.format(tmp))
+    dirs = os.listdir(file)
+    f = []
+    for d in dirs:
+        if d[-3:] == 'aif':
+            f.append(d)
+    dirs = f
+
+    # load data as sample information kept in np.array
+    kick_data, samplerate = sf.read('audio/{}'.format(dirs[5]))
+    snare_data, _         = sf.read('audio/{}'.format(dirs[6]))
+    tom_h_data, _         = sf.read('audio/{}'.format(dirs[1]))
+    tom_m_data, _         = sf.read('audio/{}'.format(dirs[4]))
+    tom_l_data, _         = sf.read('audio/{}'.format(dirs[3]))
+    ohl_data, _           = sf.read('audio/{}'.format(dirs[2]))
+    ohr_data, _           = sf.read('audio/{}'.format(dirs[0]))
+
+    # convert from samples to db using local root mean square convolution
+    kick_data  = window_rms(kick_data, samplerate / (frame_rate * window_multiplier))
+    snare_data = window_rms(snare_data, samplerate / (frame_rate * window_multiplier))
+    tom_h_data = window_rms(tom_h_data, samplerate / (frame_rate * window_multiplier))
+    tom_m_data = window_rms(tom_m_data, samplerate / (frame_rate * window_multiplier))
+    tom_l_data = window_rms(tom_l_data, samplerate / (frame_rate * window_multiplier))
+    ohl_data   = window_rms(ohl_data, samplerate / (frame_rate * window_multiplier))
+    ohr_data   = window_rms(ohr_data, samplerate / (frame_rate * window_multiplier))
+
+    # normalize and select numbers conducive to frame rate
+    data = np.stack([kick_data[0::int(samplerate / frame_rate)] / kick_data.max(),
+                     snare_data[0::int(samplerate / frame_rate)] / snare_data.max(),
+                     tom_h_data[0::int(samplerate / frame_rate)] / tom_h_data.max(),
+                     tom_m_data[0::int(samplerate / frame_rate)] / tom_m_data.max(),
+                     tom_l_data[0::int(samplerate / frame_rate)] / tom_l_data.max(),
+                     ohl_data[0::int(samplerate / frame_rate)] / ohl_data.max(),
+                     ohr_data[0::int(samplerate / frame_rate)] / ohr_data.max()],
+                    axis=1)
+
+    ##############################################################################
+    ## At this point the data is clean, idx'd by frame and needs to be chunked  ##
+    ##      data: np.ndarray (n_samples / (samplerate / frame_rate), 7)         ##
+    ##############################################################################
+
+    # printing shape of data to confirm that the quantity at line 86 is appx num_seconds
+    print(data.shape)
+    print((data.shape[0] - data.shape[0] % frame_rate) / frame_rate)
+
+    # chunking for processes
+    count = 0
+    with open('{}/parallel_script_list.txt'.format(tmp), 'w') as f:
+        for start in range(0, data.shape[0], frame_rate):
+            # this makes sure that the animations are smooth
+            if start + frame_rate + 1 > kick_data.size:
+                stop = kick_data.size
+            else:
+                stop = start + frame_rate + 1
+
+            # won't err due to os/python separation (will print on stderr)
+            os.system('mkdir tmp/media/{}'.format(count))
+
+            # write chunk to file
+            var_to_file(data[start:stop, :], "{}/{}data.bin".format(tmp, count))
+
+            # listening to snarky puppy figuring shit out like a boss song_title='outlier'
+            if count == 0:
+                f.write('python -m manim {} --disable_caching --music_file={}/{}data.bin -first=true --media_dir={}/{}/{} main.py Video > debug/segment_{}.log\n'
+                        .format(qual_flag, tmp, count, tmp, 'media', count, count))
+            else:
+                f.write('python -m manim {} --disable_caching --music_file={}/{}data.bin --media_dir={}/{}/{} main.py Video > debug/segment_{}.log\n'
+                        .format(qual_flag, tmp, count, tmp, 'media', count, count))
+            count += 1
+
+    # render the video segments
+    os.system('sh /Users/scarymonkey83/PycharmProjects/manim_rtvf383_final/multiprocess.sh /Users/scarymonkey83/PycharmProjects/manim_rtvf383_final/{}/parallel_script_list.txt {}'.format(tmp, n_cores))
+
+    # stitch segments together
+    clips = []
+    for num in range(count):
+        media_dir = 'tmp/media/{}/videos/main/480p15/Video.mp4'
+        clips.append(VideoFileClip(media_dir))
+    final_video = concatenate_videoclips(clips)
+    final_video.write_videofile("final_video.mp4")
+    os.system('rm -r {}'.format(tmp))
+    print("Runtime = {}".format(datetime.now() - now))
