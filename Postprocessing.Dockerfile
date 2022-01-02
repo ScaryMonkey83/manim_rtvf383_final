18eec1c4435182105d0b3b4f1f9ba85a8fbc8dc7
jdetwiler <jacob.detwiler@theperducogroup.com>
2022-01-02 00:52:24 -0600

    post processing lambda setup

---
+++ Postprocessing.Dockerfile
@@ -0,0 +1,27 @@
+FROM ubuntu:20.04
+MAINTAINER jalehndet11@gmail.com
+SHELL ["/bin/bash", "-c"]
+
+RUN apt-get update
+RUN apt-get install -y apt-utils
+RUN apt-get install -y python3.9
+RUN apt-get install -y python3.9-venv
+RUN apt-get install -y g++
+RUN apt-get install -y python3.9-dev
+RUN apt-get install -y ffmpeg
+RUN apt-get install -y inotify-tools
+RUN apt-get install -y wget
+
+RUN mkdir "manim_rtvf383_final"
+WORKDIR manim_rtvf383_final
+RUN wget https://raw.githubusercontent.com/ScaryMonkey83/manim_rtvf383_final/continuation/stitch.py
+RUN wget https://raw.githubusercontent.com/ScaryMonkey83/manim_rtvf383_final/continuation/requirements_preprocess.txt
+RUN wget https://raw.githubusercontent.com/ScaryMonkey83/manim_rtvf383_final/continuation/scripts/begin_preprocess.sh
+
+RUN python3.9 -m venv venv
+RUN source venv/bin/activate && pip install wheel
+RUN source venv/bin/activate && pip install setuptools
+RUN source venv/bin/activate && pip install --upgrade wheel setuptools pip
+RUN source venv/bin/activate && pip install -r requirements_preprocess.txt
+
+CMD ["bash", "begin_preprocess.sh"]
--- stitch.py
+++ stitch.py
@@ -3,11 +3,37 @@ from datetime import datetime
 
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 from moviepy.video.io.VideoFileClip import VideoFileClip
+import boto3 as aws
 
-count = 89
+from manim_music import (
+    aws_region,
+    s3_bucket
+)
+
+aws_session = aws.Session()
+s3_resource = aws_session.resource('s3', aws_region)
+s3_object = s3_resource.Bucket(s3_bucket)
+
+# a client for the s3 service
+s3 = aws.client('s3')
+
+# find  the 7 audio files and stick it in a list.
+response = s3.list_objects_v2(
+    Bucket=s3_bucket,
+    Prefix='videos/',
+    MaxKeys=10000)
+dirs = [d['Key'] for d in response['Contents']]
+
 clips = []
-for num in range(count):
-    media_dir = 'tmp/media/{}/videos/main/1080p60.0/Video.mp4'.format(num)
+
+# download the final videos
+for num in range(len(dirs)):
+    media_dir = 'tmp/Video_{}.mp4'.format(num)
+    s3_object.Object('videos/Video_{}.mp4'.format(num)).download_file(media_dir)
+
+# build the final video with clips
+for num in range(len(dirs)):
+    media_dir = 'tmp/Video_{}.mp4'.format(num)
     clips.append(VideoFileClip(media_dir))
 final_video = concatenate_videoclips(clips)
 final_video.write_videofile("final_video.mp4")
